863b4faaad4fbb70e54d84da8ada8c08127a347c
carol <carolmurithi1994@gmail.com>
2016-09-28 22:18:10 +0300

    adding partials to the medi app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
-end
+  #returns the full title on a per-page basis
+  def full_title=(page_title = '')
+   base_title = "medi_app"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+    end
